351c519af73b7ccfb88c5a3598e1fdc5affc7221
Mamoona Latif <mamoonaalatif@gmail.com>
2025-12-26 23:28:34 +0500

    Initializing core infrastructure (backend/src/admin/admin.module.ts)

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { AdminService } from './admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Review } from '../entities/review.entity';
-import { Log } from '../entities/log.entity';
-
+import { Watchlist } from '../entities/watchlist.entity';
+import { ApiLog } from '../entities/api-log.entity';
+import { LoginLog } from '../entities/login-log.entity';
+import { Subscription } from '../entities/subscription.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Review, Log])],
+  imports: [TypeOrmModule.forFeature([User, Review, Watchlist, ApiLog, LoginLog, Subscription])],
   controllers: [AdminController],
   providers: [AdminService],
 })
-export class AdminModule {}
+export class AdminModule { }
