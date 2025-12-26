d2a729e7cef85d063e34bbc15a2bcdbbba0a44bd
Mamoona Latif <mamoonaalatif@gmail.com>
2025-12-26 23:45:16 +0500

    Security: Secure JWT Authentication & Access Control (backend/src/auth/auth.module.ts)

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { LogsModule } from '../logs/logs.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from '../entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') || 'secret',
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+    forwardRef(() => LogsModule),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
