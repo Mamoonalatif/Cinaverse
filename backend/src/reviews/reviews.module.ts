9c26b8913c60b98cd4e663e5cd6412005973bb21
Mamoona Latif <mamoonaalatif@gmail.com>
2025-12-26 23:28:38 +0500

    Initializing core infrastructure (backend/src/reviews/reviews.module.ts)

--- backend/src/reviews/reviews.module.ts
+++ backend/src/reviews/reviews.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Review } from '../entities/review.entity';
+import { LogsModule } from '../logs/logs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [TypeOrmModule.forFeature([Review]), forwardRef(() => LogsModule)],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
