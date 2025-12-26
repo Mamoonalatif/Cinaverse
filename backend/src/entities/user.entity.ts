38ac894b7afdf541eab9931710f8121280f8a0ac
Mamoona Latif <mamoonaalatif@gmail.com>
2025-12-26 23:28:37 +0500

    Mapping Core Database Schema & Relationships (backend/src/entities/user.entity.ts)

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -2,12 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Watchlist } from './watchlist.entity';
 import { Review } from './review.entity';
 import { Subscription } from './subscription.entity';
-import { Log } from './log.entity';
+import { LoginLog } from './login-log.entity';
+import { ApiLog } from './api-log.entity';
+import { ChildProfile } from './child-profile.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
 
   @Column({ unique: true })
   email: string;
@@ -27,6 +36,15 @@ export class User {
   @OneToMany(() => Subscription, (s) => s.user)
   subscriptions: Subscription[];
 
-  @OneToMany(() => Log, (l) => l.user)
-  logs: Log[];
+  @OneToMany(() => LoginLog, (l) => l.user)
+  loginLogs: LoginLog[];
+
+  @OneToMany(() => ApiLog, (a) => a.user)
+  apiLogs: ApiLog[];
+
+  @OneToMany(() => ChildProfile, (childProfile) => childProfile.parent)
+  childProfiles: ChildProfile[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 }
