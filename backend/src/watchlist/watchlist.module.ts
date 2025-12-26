6f2d214d3ee276041dfbbac620c3e37f68736fee
Mamoona Latif <mamoonaalatif@gmail.com>
2025-12-26 23:28:39 +0500

    Initializing core infrastructure (backend/src/watchlist/watchlist.module.ts)

--- backend/src/watchlist/watchlist.module.ts
+++ backend/src/watchlist/watchlist.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WatchlistController } from './watchlist.controller';
 import { WatchlistService } from './watchlist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Watchlist } from '../entities/watchlist.entity';
+import { LoginLogService } from '../logs/login-log.service';
+import { ApiLogService } from '../logs/api-log.service';
+import { LoginLog } from '../entities/login-log.entity';
+import { ApiLog } from '../entities/api-log.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Watchlist])],
+  imports: [
+    TypeOrmModule.forFeature([Watchlist, LoginLog, ApiLog]),
+  ],
   controllers: [WatchlistController],
-  providers: [WatchlistService],
+  providers: [WatchlistService, LoginLogService, ApiLogService],
 })
 export class WatchlistModule {}
